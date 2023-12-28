08755560151fc5662e5108477ed8883427911af9
Tony Oh <do256@cornell.edu>
2023-12-29 04:46:57 +0900

    Implemented various features
    - Implemented random words generator
    - Implemented typing input
    - Implemented redo button to reset words

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-// import './assets/sass/style.scss'
-
-// npm start
+import MainPage from './pages/MainPage';
 
 function App() {
-  const [textInput, setTextInput] = useState('');
-
-  const handleInputChange = (event) => {
-    setTextInput(event.target.value);
-  };
-
   return (
     <div className="App">
-      <div className="Box-container">
-        <div className="Box-content">
-          <p>Random Text
-          </p>
-          <p>Typed: {textInput}</p>
-          <input
-            type="text"
-            value={textInput}
-            onChange={handleInputChange}
-            placeholder=""
-          />
-        </div>
-      </div>
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/MainPage.js
@@ -0,0 +1,78 @@
+import { useState, useEffect, useCallback } from 'react';
+import './MainPage.css';
+// https://github.com/dariusk/corpora/blob/master/data/words/common.json
+import wordsData from '../assets/words.json';
+
+
+const MainPage = () => {
+
+  const [textInput, setTextInput] = useState('')
+
+  const [randList, setRandList] = useState([])
+
+  // const [typedList, setTypedList] = useState([])
+
+  const getRandWord = (language) => {
+    const words = wordsData[language] || [];
+    const word = words[Math.floor(Math.random() * words.length)];
+    // console.log(word)
+    return word
+  }
+
+  const initRandList = useCallback((language) => {
+    const set = new Set();
+    while (set.size < 10) {
+      const word = getRandWord("english");
+      set.add(word)
+    }
+    const array = Array.from(set);
+    setRandList(array);
+  }, [])
+
+  const initRandListString = () => {
+    const randListString = randList.join(" ")
+    return randListString
+  }
+
+  const handleTextChange = (event) => {
+    if ((event.target.value).endsWith(" ")) {
+      setTextInput("")
+    } else {
+      setTextInput(event.target.value)
+    }
+    // console.log(event.target.value) // prints the attempted word
+  }
+
+  function handleRedoClick() {
+    initRandList("english");
+  }
+
+  function RedoButton() {
+    return (<button className="Redo-button" onClick={handleRedoClick}>redo</button>);
+  }
+
+  useEffect(() => {
+    initRandList("english");
+  }, [initRandList]);
+
+  return (
+    <><h1 className='Title'>tupe!</h1>
+      <div className="Box-container">
+
+        <div className="Box-content">
+          <p>{initRandListString()}</p>
+          <input
+            type="text"
+            value={textInput}
+            onChange={handleTextChange}
+            placeholder=""
+          />
+        </div> <RedoButton />
+      </div>
+
+    </>
+
+  )
+}
+
+export default MainPage
